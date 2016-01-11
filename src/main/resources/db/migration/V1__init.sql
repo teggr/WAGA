99ea8c8d35b3c402b62e61aa7742de27a931309b
Robin Tegg <robin@tegg.me.uk>
2016-01-11 23:03:41 +0000

    Add alway scripts

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,11 @@
+drop table news_item if exists;
+
+drop table player if exists;
+
+drop table race_to_ciao_bella if exists;
+
+drop table result if exists;
+
+drop table tournament if exists;
+
+drop table venue if exists;
