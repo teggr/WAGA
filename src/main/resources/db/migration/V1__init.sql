b20dc6a05437022796c7befe05d0510b1fede7cd
Robin Tegg <robin@tegg.me.uk>
2016-01-12 01:02:16 +0000

    update schema

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -7,20 +7,20 @@
 
 -- Started on 2016-01-12 00:55:15 GMT
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
+--SET statement_timeout = 0;
+--SET lock_timeout = 0;
+--SET client_encoding = 'UTF8';
+--SET standard_conforming_strings = on;
+--SET check_function_bodies = false;
+--SET client_min_messages = warning;
+--SET row_security = off;
 
 --
 -- TOC entry 179 (class 3079 OID 12749)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+--CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
