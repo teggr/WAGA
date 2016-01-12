37c9e4f35562c34075c63f083fc57ed445804479
Robin Tegg <robin@tegg.me.uk>
2016-01-12 00:08:18 +0000

    change id generation to sequences

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,11 +1,6 @@
 drop table if exists news_item ;
-
 drop table if exists player;
-
 drop table if exists race_to_ciao_bella;
-
 drop table if exists result;
-
 drop table if exists tournament;
-
 drop table if exists venue;
