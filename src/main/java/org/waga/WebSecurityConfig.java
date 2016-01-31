75ed0006db9965262a1da9eec09c121ef6af876f
Robin Tegg <robin@tegg.me.uk>
2016-01-31 10:06:24 +0000

    pass credentials in through heroic

--- src/main/java/org/waga/WebSecurityConfig.java
+++ src/main/java/org/waga/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package org.waga;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,21 +11,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	public static class AdminCredentials {
+		private String username;
+		private String password;
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+	}
+
+	@ConfigurationProperties("waga.admin.security")
+	AdminCredentials adminCredentials() {
+		return new AdminCredentials();
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http
-			.authorizeRequests()
-				.antMatchers("/admin/**")
-					.authenticated()
-			.and()
-				.formLogin().permitAll()
-			.and()
-				.logout().permitAll();
+		http.authorizeRequests().antMatchers("/admin/**").authenticated().and().formLogin().permitAll().and().logout()
+				.permitAll();
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("wagmin").password("woodmancup").roles("USER");
+		auth.inMemoryAuthentication().withUser(adminCredentials().getUsername())
+				.password(adminCredentials().getUsername()).roles("USER");
 	}
 }
