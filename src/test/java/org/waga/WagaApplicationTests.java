24b129a74df52694428f1ba9c36ed0ab091927f2
Robin Tegg <robin@tegg.me.uk>
2016-02-24 14:39:51 +0000

    ignore tests

--- src/test/java/org/waga/WagaApplicationTests.java
+++ src/test/java/org/waga/WagaApplicationTests.java
@@ -1,5 +1,6 @@
 package org.waga;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -11,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @WebAppConfiguration
 public class WagaApplicationTests {
 
+	@Ignore
 	@Test
 	public void contextLoads() {
 	}
