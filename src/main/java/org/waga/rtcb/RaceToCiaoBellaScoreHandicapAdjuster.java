8d11f8d962c0379460de7693cc7308b44b54adfc
Robin Tegg <robin@tegg.me.uk>
2016-07-24 16:40:13 +0100

    added logging

--- src/main/java/org/waga/rtcb/RaceToCiaoBellaScoreHandicapAdjuster.java
+++ src/main/java/org/waga/rtcb/RaceToCiaoBellaScoreHandicapAdjuster.java
@@ -3,11 +3,17 @@ package org.waga.rtcb;
 import java.util.List;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.waga.player.Player;
 
 public class RaceToCiaoBellaScoreHandicapAdjuster {
 
+	private static final Logger log = LoggerFactory.getLogger(RaceToCiaoBellaScoreHandicapAdjuster.class);
+
 	public void apply(RaceToCiaoBella raceToCiaoBella) {
+
+		log.info("Adjusting handicaps based on Race to cia bella scores");
 
 		List<Tournament> tournaments = raceToCiaoBella.getTournamentsSortedByDate();
 
@@ -17,11 +23,17 @@ public class RaceToCiaoBellaScoreHandicapAdjuster {
 			for (Result result : results) {
 				Player p = result.getPlayer();
 				if (result.getScore() >= 40) {
-					p.decrementHandicap(2, "40 points or over");
+					String message = "40 points or over";
+					log.info(p.getFullName() + " adjusted " + message);
+					p.decrementHandicap(2, message);
 				} else if (result.getScore() >= 38) {
-					p.decrementHandicap(1, "38 points or over");
+					String message = " adjusted 38 points or over";
+					log.info(p.getFullName() + " " + message);
+					p.decrementHandicap(1, message);
 				} else if (result.getScore() <= 27) {
-					p.incrementHandicap(1, "27 points or less");
+					String message = " adjusted 27 points or less";
+					log.info(p.getFullName() + " " + message);
+					p.incrementHandicap(1, message);
 				}
 
 			}
--- src/main/java/org/waga/rtcb/RaceToCiaoBellaWinningHandicapAdjuster.java
+++ src/main/java/org/waga/rtcb/RaceToCiaoBellaWinningHandicapAdjuster.java
@@ -3,14 +3,17 @@ package org.waga.rtcb;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.waga.player.Player;
 
 public class RaceToCiaoBellaWinningHandicapAdjuster {
 
-	private Date date;
+	private static final Logger log = LoggerFactory.getLogger(RaceToCiaoBellaWinningHandicapAdjuster.class);
+	private Date applyFromDate;
 
 	public RaceToCiaoBellaWinningHandicapAdjuster(Date applyFromDate) {
-		this.date = applyFromDate;
+		this.applyFromDate = applyFromDate;
 	}
 
 	public void apply(RaceToCiaoBella raceToCiaoBella) {
@@ -19,10 +22,12 @@ public class RaceToCiaoBellaWinningHandicapAdjuster {
 
 		for (Tournament tournament : tournaments) {
 
-			if (tournament.getDate().compareTo(date) > 0) {
+			if (tournament.getDate().after(applyFromDate)) {
 
 				Player p = tournament.getWinner();
-				p.decrementHandicap(1, "Tournament winner");
+				String message = "Tournament winner";
+				log.info(p.getFullName() + " adjusted " + message);
+				p.decrementHandicap(1, message);
 
 			}
 
